19ae405a6c23fcfe045b2af5560ddc06e1ad3023
dev-xiligroup <contact@xiligroup.com>
2014-11-16 16:58:56 +0100

    basic files

---
+++ searchform.php
@@ -0,0 +1,21 @@
+<?php 
+	if ( 'html5' == $format ) {
+		$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
+			<label>
+				<span class="screen-reader-text">' . _x( 'Search for:', 'label', 'twentyfifteen' ) . '</span>
+				<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyfifteen' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label', 'twentyfourteen' ) . '" />
+			</label>
+			<input type="submit" class="search-submit" value="'. esc_attr_x( 'Search', 'submit button', 'twentyfifteen' ) .'" />
+		</form>';
+	} else {
+		$form = '<form role="search" method="get" id="searchform" class="searchform" action="' . esc_url( home_url( '/' ) ) . '">
+			<div>
+				<label class="screen-reader-text" for="s">' . _x( 'Search for:', 'label', 'twentyfifteen' ) . '</label>
+				<input type="text" value="' . get_search_query() . '" name="s" id="s" />
+				<input type="submit" id="searchsubmit" value="'. esc_attr_x( 'Search', 'submit button', 'twentyfifteen' ) .'" />
+			</div>
+		</form>';
+	}
+	echo $form;
+
+?>
