2bfce9e9d60bddc4df75de747a3eb3b551fb9f81
dev-xiligroup <contact@xiligroup.com>
2014-12-16 09:04:45 +0100

    search domain fixed

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 		$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
 			<label>
 				<span class="screen-reader-text">' . _x( 'Search for:', 'label', 'twentyfifteen' ) . '</span>
-				<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyfifteen' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label', 'twentyfourteen' ) . '" />
+				<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyfifteen' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label', 'twentyfifteen' ) . '" />
 			</label>
 			<input type="submit" class="search-submit" value="'. esc_attr_x( 'Search', 'submit button', 'twentyfifteen' ) .'" />
 		</form>';
